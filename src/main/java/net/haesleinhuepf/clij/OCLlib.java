5bbe765ecb5c875d2fca43a1569356618cd1957e
haesleinhuepf <rhaase@mpi-cbg.de>
2019-10-24 11:29:40 +0200

    renamed/moved KernelAnchor to OCLlib

---
+++ src/main/java/net/haesleinhuepf/clij/OCLlib.java
@@ -0,0 +1,12 @@
+package net.haesleinhuepf.clij;
+
+/**
+ * The OCLlib class just serves as anchor for the .cl
+ * files in the same package. It makes them accessible using ClearCL.
+ *
+ * Author: @haesleinhuepf
+ *         October 2019
+ */
+public final class OCLlib {
+    private OCLlib() {}
+}

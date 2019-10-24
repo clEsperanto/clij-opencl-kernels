6c0dc4fd28fdaa622a9d97ad93cef813a036b69e
haesleinhuepf <rhaase@mpi-cbg.de>
2019-10-24 11:04:57 +0200

    add anchor class to make cl files accessible form ClearCL

---
+++ src/main/java/net/haesleinhuepf/clij/kernels/KernelAnchor.java
@@ -0,0 +1,12 @@
+package net.haesleinhuepf.clij.kernels;
+
+/**
+ * The KernelAnchor class just serves as anchor for the .cl
+ * files in the same package. It makes them accessible using ClearCL.
+ *
+ * Author: @haesleinhuepf
+ *         October 2019
+ */
+public final class KernelAnchor {
+    private KernelAnchor() {}
+}
